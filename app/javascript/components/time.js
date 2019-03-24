e588274a4b8df26f15969a6af725be455464bb88
Xavier PICHETTI <pichon.pich@free.fr>
2019-03-25 00:02:39 +0100

    save

--- app/javascript/components/time.js
+++ app/javascript/components/time.js
@@ -1,9 +1,9 @@
 const updatetime = () => {
   let x = setInterval(function() { 
-    const today   = new Date(); 
-    const hours   = today.getHours(); 
+    const today = new Date(); 
+    const hours = today.getHours(); 
     const minutes = today.getMinutes(); 
-    let timeValue = hours;
+    let timeValue = ((hours < 10) ? "0" : "") + hours;
     timeValue += ((minutes < 10) ? ":0" : ":") + minutes;
     document.getElementById("time").innerHTML = timeValue; 
   }, 1000);
